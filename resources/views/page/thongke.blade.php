f8f3253d72bd670851768adacc4005ecae5c8d8e
= <=>
2018-03-10 01:48:25 +0700

    edit thong ke

--- resources/views/page/thongke.blade.php
+++ resources/views/page/thongke.blade.php
@@ -1,16 +1,18 @@
 @extends('master')
 @section('content')
 <div class="container-fluid">
-	
-		<div class="col-lg-6">
-			<div class="card">
-				<div class="card-body">
-					<h4 class="mb-3">Bar chart </h4>
-					<canvas id="barChart"></canvas>
+	<div class="animated fadeIn">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="mb-3">Bar chart </h4>
+						<canvas id="barChart"></canvas>
+					</div>
 				</div>
-			</div>
-		</div><!-- /# column -->
-
+			</div><!-- /# column -->
+		</div>
+	</div>
 </div>
 
 @endsection
